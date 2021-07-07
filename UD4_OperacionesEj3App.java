ae787c837bea83efc091ef2515792f0cf0714948
DavidRiveraArtero <davidtoy222@gmail.com>
2021-07-07 10:34:47 +0200

    UD4-Ej3

---
+++ UD4_OperacionesEj3App.java
@@ -0,0 +1,58 @@
+
+public class UD4_OperacionesEj3App {
+
+	public static void main(String[] args) {
+		
+		int X = 4;
+		
+		int Y = 3;
+		
+		double N = 6.2;
+		
+		double M = 10.3;
+		
+		//OPERACIONES 
+		
+		System.out.println("LA SUMA ES: " +(X + Y));
+		
+		System.out.println("LA RESTA ES: " +(X - Y));
+		
+		System.out.println("LA MULTIPLICACION ES: " +(X * Y));
+		
+		System.out.println("LA DIVISION ES: " +(X / Y));
+		
+		System.out.println("EL RESTO ES: " +(X % Y));
+		
+		System.out.println("==================================");
+
+		System.out.println("LA SUMA ES: " +(N + M));
+		
+		System.out.println("LA RESTA ES: " +(N - M));
+		
+		System.out.println("EL PRODUCTO ES: " +(N * M));
+		
+		System.out.println("LA DIVISION ES: " +(N / M));
+		
+		System.out.println("EL RESTO ES: " +(N % M));
+		
+		System.out.println("EL RESTO ES: " +(N % M));
+		
+		System.out.println("==================================");
+		
+		System.out.println("LA SUMA ES: " +(X + N));
+		
+		System.out.println("EL COCIENTE ES: " +(Y / M));
+		
+		System.out.println("EL RESTO ES: " +(Y % M));
+		
+		System.out.println("==================================");
+		
+		System.out.println("EL DOBLE DE CADA VARIABLE ES: " +((X * 2) +" " + (Y * 2) +" " + (N * 2) + " " + (M * 2)));
+		
+		System.out.println("LA SUMA DE TODAS LAS VARIABLE ES: " +(X + Y + N + M));
+		
+		System.out.println("EL PRODUCTO DE TODAS LAS VARIABLE ES: " +(X * Y * N * M));
+		
+	}
+
+}
